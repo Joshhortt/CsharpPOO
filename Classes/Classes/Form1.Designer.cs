c4566456d7929f2365698101353f336c59ba3428
Joshhortt <joshhortt@yahoo.com>
2021-02-23 13:18:12 +0000

    Adicionar classe 'Pessoa' com 3 propriedades publicas e um botao e uma label no formulario.

--- Classes/Classes/Form1.Designer.cs
+++ Classes/Classes/Form1.Designer.cs
@@ -29,13 +29,48 @@ namespace Classes
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.BtnOutput = new System.Windows.Forms.Button();
+			this.labelOutput = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// BtnOutput
+			// 
+			this.BtnOutput.Location = new System.Drawing.Point(101, 227);
+			this.BtnOutput.Name = "BtnOutput";
+			this.BtnOutput.Size = new System.Drawing.Size(75, 23);
+			this.BtnOutput.TabIndex = 0;
+			this.BtnOutput.Text = "OK";
+			this.BtnOutput.UseVisualStyleBackColor = true;
+			this.BtnOutput.Click += new System.EventHandler(this.BtnOutput_Click);
+			// 
+			// labelOutput
+			// 
+			this.labelOutput.AutoSize = true;
+			this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelOutput.Location = new System.Drawing.Point(100, 124);
+			this.labelOutput.Name = "labelOutput";
+			this.labelOutput.Size = new System.Drawing.Size(76, 25);
+			this.labelOutput.TabIndex = 1;
+			this.labelOutput.Text = "Output";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(290, 358);
+			this.Controls.Add(this.labelOutput);
+			this.Controls.Add(this.BtnOutput);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button BtnOutput;
+		private System.Windows.Forms.Label labelOutput;
 	}
 }
 
--- Classes/Classes/Form1.cs
+++ Classes/Classes/Form1.cs
@@ -16,5 +16,24 @@ namespace Classes
 		{
 			InitializeComponent();
 		}
+
+		private void BtnOutput_Click(object sender, EventArgs e)
+		{
+			Pessoa pessoa1 = new Pessoa();  // criar nova instancia
+			pessoa1.nome = "Luis";
+			pessoa1.idade = 39;
+			pessoa1.corCabelo = "castanho";
+
+			Pessoa pessoa2 = new Pessoa();  // criar nova instancia
+			pessoa2.nome = "Maximiliano";
+			pessoa2.idade = 79;
+			pessoa2.corCabelo = "branco";
+
+			labelOutput.Text = pessoa2.nome;  
+			// mostra ao clicar no botao na label a nova instancia criada pessoa2, o seu nome
+
+		    //labelOutput.Text = pessoa2.corCabelo;
+
+		}
 	}
 }
