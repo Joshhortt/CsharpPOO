e769b8ff4f537bec0bc0b2192eaa556d86d3bba9
Joshhortt <joshhortt@yahoo.com>
2021-02-23 16:50:17 +0000

    Adicionar um metodo privado sem retorno de valor, com acesso dentro da classe, mostrando messageBox.

--- 03-MetodosClasses/Classes/Form1.cs
+++ 03-MetodosClasses/Classes/Form1.cs
@@ -22,6 +22,7 @@ namespace Classes
 			Pessoa novaPessoa = new Pessoa();  // 3. criar nova instancia
 			novaPessoa.Falar();  // 4. Estou a chamar a execução do metodo Falar()
 			novaPessoa.Correr();  // 7.Estou a chamar a execução do metodo Correr()
+			// novaPessoa.Saltar();  //10. Estou a chamar a execução do metodo Correr() (sem acesso - private)
 		}
 	}
 }
--- 03-MetodosClasses/Classes/Pessoa.cs
+++ 03-MetodosClasses/Classes/Pessoa.cs
@@ -10,11 +10,18 @@ namespace Classes
 	{  
 		public void Falar()  //1. metodo sem qualuqer retorno de valor. 
 		{
-			System.Windows.Forms.MessageBox.Show("Falar.");   //2. MessageBox coma mensagem
+			System.Windows.Forms.MessageBox.Show("Falar.");   //2. MessageBox com a mensagem
 		}
 		public void Correr() //5. metodo sem qualuqer retorno de valor. 
 		{
-			System.Windows.Forms.MessageBox.Show("Correr.");   //6. MessageBox coma mensagem
+			System.Windows.Forms.MessageBox.Show("Correr.");   //6. MessageBox com a mensagem
+			Saltar();    //11. O metodo privado  Saltar() faz parte desta classe Pessoa, 
+			             //por isso é possivel chamá-lo daqui e desta forma mesmo sendo privado
+		}
+
+		private void Saltar()  //8. metodo sem qualuqer retorno de valor.
+		{
+			System.Windows.Forms.MessageBox.Show("Saltar.");  //9. MessageBox com a mensagem
 		}
 
 	}
