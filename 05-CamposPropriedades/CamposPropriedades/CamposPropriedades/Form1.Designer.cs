b67187a72198334adb78b382c8b7fbb33f937f00
Joshhortt <joshhortt@yahoo.com>
2021-02-24 17:12:00 +0000

    Primeiro commit. Adicionar classe 'Teste' e um 'form' com um 'botao', um 'label', e uma 'textBox'.

---
+++ 05-CamposPropriedades/CamposPropriedades/CamposPropriedades/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace CamposPropriedades
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.BtnOk = new System.Windows.Forms.Button();
+			this.Nome = new System.Windows.Forms.Label();
+			this.textBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// BtnOk
+			// 
+			this.BtnOk.Location = new System.Drawing.Point(70, 124);
+			this.BtnOk.Name = "BtnOk";
+			this.BtnOk.Size = new System.Drawing.Size(75, 23);
+			this.BtnOk.TabIndex = 0;
+			this.BtnOk.Text = "OK";
+			this.BtnOk.UseVisualStyleBackColor = true;
+			this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click);
+			// 
+			// Nome
+			// 
+			this.Nome.AutoSize = true;
+			this.Nome.Location = new System.Drawing.Point(67, 57);
+			this.Nome.Name = "Nome";
+			this.Nome.Size = new System.Drawing.Size(35, 13);
+			this.Nome.TabIndex = 1;
+			this.Nome.Text = "Nome";
+			// 
+			// textBox
+			// 
+			this.textBox.Location = new System.Drawing.Point(70, 85);
+			this.textBox.Name = "textBox";
+			this.textBox.Size = new System.Drawing.Size(100, 20);
+			this.textBox.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(258, 255);
+			this.Controls.Add(this.textBox);
+			this.Controls.Add(this.Nome);
+			this.Controls.Add(this.BtnOk);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button BtnOk;
+		private System.Windows.Forms.Label Nome;
+		private System.Windows.Forms.TextBox textBox;
+	}
+}
+
---
+++ 05-CamposPropriedades/CamposPropriedades/CamposPropriedades/Program.cs
@@ -0,0 +1,24 @@
+﻿//05. Campos e Propriedades de uma Classe (a sua distinção)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CamposPropriedades
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
