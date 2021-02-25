6691ee3b5d81fbbc4131a00d1a58581c83fc1ffd
Joshhortt <joshhortt@yahoo.com>
2021-02-25 10:51:41 +0000

    Primeiro commit. Criar Propriedade e um Metodo. Adicionar classe 'Normal' e criar um nova instancia.

---
+++ 08-ClasseEstatica/ClasseEstatica/ClasseEstatica/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClasseEstatica
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnOk_Click(object sender, EventArgs e)
+		{
+			Normal objeto = new Normal();  // Instancia
+			objeto.Nome = "Jose Horta"; // Atribuido o valor da propriedade Nome
+			objeto.ApresentarNome();  // objeto que vai chamar o metodo da classe
+		}
+	}
+}
---
+++ 08-ClasseEstatica/ClasseEstatica/ClasseEstatica/Normal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseEstatica
+{
+	class Normal
+	{
+
+		public string Nome { set; get; }  // A propriedade
+
+		public void ApresentarNome()  // O metodo
+		{
+			System.Diagnostics.Trace.WriteLine(Nome);
+		}
+	}
+}
