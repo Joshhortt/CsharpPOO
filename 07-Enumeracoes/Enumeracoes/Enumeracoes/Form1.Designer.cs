9b08961655bdaf3ed1ee50d17e00ad08a56336b5
Joshhortt <joshhortt@yahoo.com>
2021-02-25 09:47:53 +0000

    Primeiro commit. Criar um Metodo. Adicionar classe 'Teste' e criar 'enum' e nova instancia.

---
+++ 07-Enumeracoes/Enumeracoes/Enumeracoes/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Enumeracoes
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.BtnOk = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// BtnOk
+			// 
+			this.BtnOk.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.BtnOk.Location = new System.Drawing.Point(253, 246);
+			this.BtnOk.Name = "BtnOk";
+			this.BtnOk.Size = new System.Drawing.Size(138, 43);
+			this.BtnOk.TabIndex = 0;
+			this.BtnOk.Text = "OK";
+			this.BtnOk.UseVisualStyleBackColor = true;
+			this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(657, 450);
+			this.Controls.Add(this.BtnOk);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button BtnOk;
+	}
+}
+
---
+++ 07-Enumeracoes/Enumeracoes/Enumeracoes/Teste.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumeracoes
+{
+	class Teste
+	{
+
+		// Criar uma Enumeração (2 formas - code snippet ou manual)
+		public enum DiasDaSemana
+		{
+			domingo, segunda, terça, quarta, quinta, sexta, sabado
+		}
+
+
+		public void ApresentarDia(DiasDaSemana dia)
+		{
+			System.Diagnostics.Trace.WriteLine("O dia escolhido foi " + dia);
+		}
+	}
+}
