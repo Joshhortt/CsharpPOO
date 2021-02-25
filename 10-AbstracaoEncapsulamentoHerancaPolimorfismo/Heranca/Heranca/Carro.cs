d1954efb550f5ddc3a8a9e05afc707b36d666fc7
Joshhortt <joshhortt@yahoo.com>
2021-02-25 16:04:29 +0000

    Adicionar super classe 'Veiculo' com 2 props. Criar nova instancia 'Veiculo'. Criar classe 'Carro' uma props e que herda props do 'pai'.

---
+++ 10-AbstracaoEncapsulamentoHerancaPolimorfismo/Heranca/Heranca/Carro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+	class Carro : Veiculo  // a classe carro herda da classe veiculo
+	{
+		public int VelocidadeMaxima { get; set; }
+	}
+}
---
+++ 10-AbstracaoEncapsulamentoHerancaPolimorfismo/Heranca/Heranca/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Heranca
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void BtnExecutar_Click(object sender, EventArgs e)
+		{
+			Veiculo veiculo = new Veiculo();  // Instancia veiculo
+			
+			Carro carro = new Carro(); // Instancia Carro
+			
+			
+		}
+	}
+}
